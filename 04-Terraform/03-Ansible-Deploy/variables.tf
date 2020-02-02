82e6eaacbfbf2ae2ed774278d09e82723df59ca6
root <root@mtadminnuvepro.vs1mooahc5ee3mvlxggon0e4le.cx.internal.cloudapp.net>
2020-02-02 13:01:26 +0000

    adding terraform

---
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -0,0 +1,11 @@
+variable "key_name" {
+  default = "DevOps-301-AWS"
+}
+
+variable "pvt_key" {
+  default = "/root/.ssh/DevOps-301-AWS.pem"
+}
+
+variable "sg-id" {
+  default = "sg-0893594d3862c33be"
+}
